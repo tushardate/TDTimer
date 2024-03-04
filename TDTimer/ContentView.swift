9e4ec0060f87b68b386e039f5d44d2c89fe94544
tushardate <tushardate@gmail.com>
2024-03-04 09:21:40 -0800

    Initial Commit

---
+++ TDTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TDTimer
+//
+//  Created by TUSHAR DATE on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TDTimer/TDTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDTimerApp.swift
+//  TDTimer
+//
+//  Created by TUSHAR DATE on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TDTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
